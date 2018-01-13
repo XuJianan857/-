47aeb425571b374b3c3b68b0f5427c92b85e3492
xjn <857851654@qq.com>
2018-01-13 14:37:16 +0800

    测试1

---
+++ src/component/login/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>莫江鹏狗</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        created(){
+            this.$http.get(this.$api.islogin).then(res => {
+                console.log(res.data);
+            });
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
-import App from './component/App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+import App from './component/App.vue';
+import routerConfig from './router/index.js';
+import axios from 'axios';
+axios.defaults.baseURL='http://157.122.54.189:9095';
+Vue.prototype.$http = axios;
+
+import api from './js/api.config';
+Vue.prototype.$api = api;
+
 new Vue({
     el: '#app',
     render: function(createElement) {
         return createElement(App);
-    }
+    },
+    router:new VueRouter(routerConfig)
 })
